19be7666984a864aaf26afeb39371ade1d3519df
Adrian <89000416+AdrianJGonzalez@users.noreply.github.com>
2025-02-20 22:55:56 -0500

    Add files via upload

---
+++ courses.py
@@ -0,0 +1,161 @@
+courses = {
+    "University of South Florida": {
+        "ACG": {
+            "2021": {
+                "Class Full Name": "Principles of Financial Accounting",
+                "Description": "Study of basic accounting principles including the recording and reporting of financial activity. The preparation and interpretation of financial statements.",
+                "CoReqs": "N/A",
+                "Mingrade_coreq": "N/A",
+                "Prereqs": "N/A",
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            },
+            "2071": {
+                "Class Full Name": "Principles of Managerial Accounting",
+                "Description": "A study of the accountant\u2019s role in assisting management in the planning and controlling of business activities.",
+                "CoReqs": "N/A",
+                "Mingrade_coreq": "N/A",
+                "Prereqs": [
+                    {
+                        "Department": "ACG",
+                        "Course Code": "2021",
+                        "Grade": "C"
+                    }
+                ],
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            },
+            "3074": {
+                "Class Full Name": "Managerial Accounting for Non-Business Majors",
+                "Description": "The study of the uses of accounting data internally by managers in planning and controlling the affairs of organizations.",
+                "CoReqs": "N/A",
+                "Mingrade_coreq": "N/A",
+                "Prereqs": "N/A",
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            },
+            "3103": {
+                "Class Full Name": "Intermediate Financial Accounting I",
+                "Description": "Theory and methodology underlying financial reporting, including the FASB\u2019s conceptual framework, the accounting process, financial statements, accounting changes, present value applications, and current assets.",
+                "CoReqs": [
+                    {
+                        "Department": "ACG",
+                        "Course Code": "3341",
+                        "Grade": "C"
+                    }
+                ],
+                "Mingrade_coreq": "N/A",
+                "Prereqs": "N/A",
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            },
+            "3113": {
+                "Class Full Name": "Intermediate Financial Accounting II",
+                "Description": "Continuation of ACG 3103. Topics covered include property, plant and equipment, intangibles, current liabilities, long-term debt, leases, tax allocation, statement of cash flows.",
+                "CoReqs": "N/A",
+                "Prereqs": [
+                    {
+                        "Department": "ACG",
+                        "Course Code": "3103",
+                        "Grade": "C"
+                    },
+                    [
+                        {
+                            "Department": "ACG",
+                            "Course Code": "3341",
+                            "Grade": "C"
+                        },
+                        {
+                            "Department": "ACG",
+                            "Course Code": "3401",
+                            "Grade": "C"
+                        },
+                        {
+                            "Department": "TAX",
+                            "Course Code": "4001",
+                            "Grade": "C"
+                        }
+                    ]
+                ],
+                "Credit Hours": 3
+            },
+            "3341": {
+                "Class Full Name": "Cost Accounting and Control I",
+                "Description": "Deals with cost accounting systems for different entities, cost behavior patterns, cost-volume-profit analysis, relevant information for decision making, and budgets and standard costs for planning and control.",
+                "Prereqs": {
+                    "AND": [
+                        {
+                            "OR": [
+                                {
+                                    "Department": "ACG",
+                                    "Course Code": "2021",
+                                    "Grade": "C"
+                                },
+                                {
+                                    "Department": "ACG",
+                                    "Course Code": "2021C",
+                                    "Grade": "C"
+                                },
+                                {
+                                    "AND": [
+                                        {
+                                            "Department": "ACG",
+                                            "Course Code": "2001",
+                                            "Grade": "C"
+                                        },
+                                        {
+                                            "Department": "ACG",
+                                            "Course Code": "2011",
+                                            "Grade": "C"
+                                        }
+                                    ]
+                                }
+                            ]
+                        },
+                        {
+                            "Department": "ACG",
+                            "Course Code": "2071",
+                            "Grade": "C"
+                        }
+                    ]
+                },
+                "Coreqs": {
+                    "AND": [
+                        {
+                            "Department": "ACG",
+                            "Course Code": "3103",
+                            "Grade": "C"
+                        }
+                    ]
+                },
+                "Credit Hours": 3
+            }
+        },
+        "EEL": {
+            "4835": {
+                "Class Full Name": "Programming Design",
+                "Description": "Python Class",
+                "CoReqs": "N/A",
+                "Mingrade_coreq": "N/A",
+                "Prereqs": [
+                    "Pro w/ C"
+                ],
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            }
+        }
+    },
+    "Hillsborough CC": {
+        "ENC": {
+            "1101": {
+                "Class Full Name": "English Composition I",
+                "Description": "English Class",
+                "CoReqs": "N/A",
+                "Mingrade_coreq": "N/A",
+                "Prereqs": "N/A",
+                "Mingrade_prereq": "N/A",
+                "Credit Hours": 3
+            }
+        }
+    }
+}
