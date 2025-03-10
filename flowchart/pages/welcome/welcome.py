4629c28d3df49f7a8ed5f6211c614fa7f7add6b7
Adrian <89000416+AdrianJGonzalez@users.noreply.github.com>
2025-03-09 23:22:40 -0400

    Delete flowchart directory

--- flowchart/pages/welcome/welcome.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-def render(surface, area_rect, font, BLACK, BLUE):
-    padding = 20
-    text = "Welcome to the Flowchart Application!"
-    text_surface = font.render(text, True, BLACK)
-    surface.blit(text_surface, (area_rect.x + padding, area_rect.y + padding))
